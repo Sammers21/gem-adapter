c93e37f982c3c57437404574651f2b3691074151
Sammers21 <titantins@gmail.com>
2020-04-27 13:28:05 +0300

    fix test case name 'submitNotImplemented' -> 'gemSubmitWorks'

--- src/test/java/com/artipie/gem/SubmitGemITCase.java
+++ src/test/java/com/artipie/gem/SubmitGemITCase.java
@@ -45,7 +45,7 @@ import org.junit.jupiter.api.Test;
 public class SubmitGemITCase {
 
     @Test
-    public void submitNotImplemented() throws IOException {
+    public void gemSubmitWorks() throws IOException {
         final Vertx vertx = Vertx.vertx();
         final int port = this.randomPort();
         final VertxSliceServer server = new VertxSliceServer(
